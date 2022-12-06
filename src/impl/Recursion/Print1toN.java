971539a91fbe361fd518c5fbb85bbb2bb96d8269
Mohammed Salman <68873632+s-aLman@users.noreply.github.com>
2022-12-06 23:59:15 +0530

    easy problems

---
+++ src/impl/Recursion/Print1toN.java
@@ -0,0 +1,16 @@
+package impl.Recursion;
+
+public class Print1toN {
+
+    static void print1toNWithRecursion(int N){
+        if(N==1){
+            System.out.print(1+" ");
+            return;
+        }
+        print1toNWithRecursion(N-1);
+        System.out.print(N+" ");
+    }
+    public static void main(String[] args) {
+        print1toNWithRecursion(14);
+    }
+}
---
+++ src/impl/Recursion/PrintNto1.java
@@ -0,0 +1,16 @@
+package impl.Recursion;
+
+public class PrintNto1 {
+
+    static void printNto1WithRecursion(int N){
+        if(N==1){
+            System.out.print(1);
+            return;
+        }
+        System.out.print(N+" ");
+        printNto1WithRecursion(N-1);
+    }
+    public static void main(String[] args) {
+        printNto1WithRecursion(15);
+    }
+}
