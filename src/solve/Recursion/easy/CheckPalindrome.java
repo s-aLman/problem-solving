e87c85bdfc78a165487d5789b249f575b8085d67
Mohammed Salman <68873632+s-aLman@users.noreply.github.com>
2022-12-07 23:07:42 +0530

    today's easy problem on recursion

---
+++ src/solve/Recursion/easy/CheckPalindrome.java
@@ -0,0 +1,21 @@
+package solve.Recursion.easy;
+
+public class CheckPalindrome {
+    static boolean checkPalindromeUsingRecursion(String str) {
+        int len = str.length();
+        if ( len == 1 )
+            return true;
+        if ( str.charAt(0) == str.charAt(len-1) ) {
+            return checkPalindromeUsingRecursion(str.substring(1,len-1));
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String str = "abacba";
+        if ( checkPalindromeUsingRecursion(str) )
+            System.out.println("String is Palindrome...");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
